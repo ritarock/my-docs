d6585190a89dceeb3910f782c2236379f036cd36
ritarock <dvryz1204@gmail.com>
2021-02-06 14:34:03 +0900

    update

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(): JSX.Element {
+  return (
+    <div>
+      <p>TOPへ戻る</p>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,47 @@
-export default function Home() {
-  return <div>HOME</div>;
+import Header from '../components/header';
+import Footer from '../components/footer';
+import { getSortedPostsData } from '../lib/posts';
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import Date from '../components/date';
+
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}): JSX.Element {
+  return (
+    <div>
+      <Header />
+      <ul>
+        {allPostsData.map(({ id, date, title }) => (
+          <li key={id}>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small>
+              <Date dateString={date} />
+              {date}
+            </small>
+          </li>
+        ))}
+      </ul>
+      <Footer />
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
