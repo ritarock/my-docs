f324bdf8166d86bfd28aa96ea210c398e81b69bf
ritarock <dvryz1204@gmail.com>
2021-05-29 16:32:26 +0900

    move dir

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-const BUILD_ARTICLES_DIR = path.join(process.cwd(), 'build-articles')
+const BUILD_ARTICLES_DIR = path.join(process.cwd(), 'articles')
 
 export function getArticlesData() {
   const summaryPath = path.join(BUILD_ARTICLES_DIR, 'summary.json')
