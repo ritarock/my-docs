b45e4948deae4479e8e56020c98828104d4561cc
ritarock <dvryz1204@gmail.com>
2021-06-06 01:38:56 +0900

    init react-markdown

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -2,26 +2,29 @@ import Header from '../../components/header'
 import { GetStaticPaths } from 'next'
 import { getArticlesIds, getArticlesData } from '../../lib/utils'
 import ReactMarkdown from 'react-markdown'
-
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-
-const CodeBlock = ({ language, value} ) => {
-  return (
-    <SyntaxHighlighter language={language}>
-      {value}
-    </SyntaxHighlighter>
-  )
-}
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {dark} from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 export default function Articles({data}) {
   const bodyContent = JSON.parse(data.fileContents).bodyContent
+
+  const components = {
+    code({node, inline, className, children, ...props}) {
+      const match = /language-(\w+)/.exec(className || '')
+      return !inline && match ? (
+        <SyntaxHighlighter style={dark} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
+      ) : (
+        <code className={className} {...props} />
+      )
+    }
+  }
 
   return (
     <div>
       <Header />
       <ReactMarkdown
         children={bodyContent}
-        components={CodeBlock}
+        components={components}
       />
     </div>
   )
