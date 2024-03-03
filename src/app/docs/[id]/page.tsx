680b781e8a1d321571c4c2f428ab12248a9d2ed6
ritarock <dvryz1204@gmail.com>
2024-03-03 13:41:09 +0900

    update

--- src/app/docs/[id]/page.tsx
+++ src/app/docs/[id]/page.tsx
@@ -2,6 +2,8 @@ import Header from "@/app/_components/header";
 import {getDocContents, getDocIDs } from "@/lib/api";
 import ReactMarkdown from "react-markdown"
 import reactGfm from "remark-gfm"
+import { monokai } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+import SyntaxHighlighter from 'react-syntax-highlighter'
 
 export async function generateStaticParams() {
   const docs = getDocIDs()
@@ -22,6 +24,36 @@ export default async function Doc({ params }: { params: { id: string } }) {
       </h1>
       <ReactMarkdown
         components={{
+          code({ className, children }) {
+            if (!className) return (<></>)
+            const name = className.replace('language-', '')
+            const lang: { name: string; setLang: string } = { name: '', setLang: '' }
+            lang.name = name.match(/\./) ? name.split('.')[1] : name
+
+            switch (lang.name) {
+              case 'go':
+                lang.setLang = 'go'
+                break
+              case 'js':
+              case 'javascript':
+                lang.setLang = 'javascript'
+                break
+            }
+
+
+            return (
+              <>
+                <code className={className}>
+                  <SyntaxHighlighter
+                    style={monokai}
+                    language={lang.setLang}
+                  >
+                    {children!.toString().replace(/\n$/, '')}
+                  </SyntaxHighlighter>
+                </code>
+              </>
+            )
+          }
         }}
         remarkPlugins={[reactGfm]}
       >
